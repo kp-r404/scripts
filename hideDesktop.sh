fdaa29cc63fbf81c2c3ea22fbf7ed4316ae082e3
kp-r404 <davidt_404@pm.me>
2025-03-10 14:52:56 -0400

    Add files via upload

---
+++ hideDesktop.sh
@@ -0,0 +1,14 @@
+#Type in Termina to enable execute function: chmod +x ~/Desktop/hideDesktop.sh
+
+
+#!/bin/bash
+
+# Ensure 'showDesktop.sh' is visible
+chflags nohidden ~/Desktop/showDesktop.sh
+
+# Hide all other desktop icons
+for file in ~/Desktop/*; do
+    if [ "$file" != "$HOME/Desktop/showDesktop.sh" ]; then
+        chflags hidden "$file"
+    fi
+done
---
+++ showDesktop.sh
@@ -0,0 +1,12 @@
+#Type in Terminal to enable execute function: chmod +x ~/Desktop/showDesktop.sh
+
+
+#!/bin/bash
+
+# Unhide all files on the desktop
+for file in ~/Desktop/*; do
+    chflags nohidden "$file"
+done
+
+# Ensure desktop icons are enabled (optional, for consistency)
+defaults write com.apple.finder CreateDesktop -bool true; killall Finder
